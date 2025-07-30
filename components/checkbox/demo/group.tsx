c69d3d3e4f09ec56aa1792b92892a3f899c6d633
HeyItsSammm <88196685+HeyItsSammm@users.noreply.github.com>
2025-07-30 10:31:04 +0800

    Update group.tsx
    Signed-off-by: HeyItsSammm <88196685+HeyItsSammm@users.noreply.github.com>

--- components/checkbox/demo/group.tsx
+++ components/checkbox/demo/group.tsx
@@ -9,9 +9,11 @@ const onChange: GetProp<typeof Checkbox.Group, 'onChange'> = (checkedValues) => 
 const plainOptions = ['Apple', 'Pear', 'Orange'];
 
 const options: CheckboxOptionType<string>[] = [
-  { label: 'Apple', value: 'Apple', className: 'label-1' },
-  { label: 'Pear', value: 'Pear', className: 'label-2' },
-  { label: 'Orange', value: 'Orange', className: 'label-3' },
+  { label: 'Apple', value: 'Appointment / Recruitment of staff members', className: 'label-1' },
+  { label: 'Pear', value: 'Supervision', className: 'label-2' },
+  { label: 'Orange', value: 'Promotion', className: 'label-3' },
+  { label: 'Lemon', value: 'Awarding of work / contracts', className: 'label-4' },
+  { label: 'Guava', value: 'Provision of other benefits', className: 'label-5' },
 ];
 
 const optionsWithDisabled: CheckboxOptionType<string>[] = [
